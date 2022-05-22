2fcdda371c0f50a3aa5e4c8fbe363ce0f58c7357
airpiazza <nick@piazzaonline.com>
2022-05-21 22:53:51 -0700

    modified to deploy to hapii aws account

--- src/client/src/services/Api.js
+++ src/client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `https://backend.cpphappiwebapp.com/`
-    // baseURL: `http://54.193.20.125:8081/`
+    baseURL: `https://server.webgazerbackend.link/`
+    // baseURL: `http://54.193.20.125:8081/` `https://backend.cpphappiwebapp.com/`
   })
 }
--- src/server/src/config/config.js
+++ src/server/src/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     password: process.env.DB_PASS || 'CPPHapii',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'aa13mnjbzyz2hfy.ciyfv2p6bkbk.us-west-1.rds.amazonaws.com', // 'hapiidb.c5hxam6fwt9f.us-west-1.rds.amazonaws.com',
+      host: process.env.HOST || 'aa1bk9bp1u7gv96.c2geutczdb6x.us-west-1.rds.amazonaws.com', // 'aa13mnjbzyz2hfy.ciyfv2p6bkbk.us-west-1.rds.amazonaws.com' 'hapiidb.c5hxam6fwt9f.us-west-1.rds.amazonaws.com',
       port: 3306,
       omitNull: true,
       timezone: 'AMERICA/LOS_ANGELES'
